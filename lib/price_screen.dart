1067e97c1546598090f1d8e6640aaf476ee857b1
User <moonchang04@naver.com>
2020-09-03 18:54:57 +0900

    비트코인

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io'show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,8 +9,47 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency= 'USD';
+
+  DropdownButton<String> androidDropdown(){
+
+    List<DropdownMenuItem<String>> dropdownItems = [];//리스트중에서도 드랍다운메뉴아이템의 스트링속성만가지고있는 리스트가 드랍다운아이템이라는 말임ㅋㅋ근데 비었음ㅋㅋ
+    for (String currency in currenciesList){//for in 루프문을 써주면 간편해요 ^^
+      var newItem = DropdownMenuItem(//동적변수var newItem은  DropdownMenuItem의 텍스트와 성질이랑 같다는거임ㅋㅋ
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);//그 빈공간에 뉴아이템을 채워주는거임ㅋㅋ
+    }
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items:dropdownItems,
+        onChanged: (value){
+          setState(() {
+            selectedCurrency= value;
+          });
+        });}
+  CupertinoPicker iOSPicker(){
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList){
+      pickerItems.add(Text(currency));
+    }
+
+   return CupertinoPicker(
+      backgroundColor: Colors.lightBlueAccent,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex){
+        print(selectedIndex);
+      },
+      children:pickerItems,
+    );}
+
+
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -41,11 +83,12 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+            color: Colors.lightBlueAccent,
+            child:Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
     );
   }
 }
+
